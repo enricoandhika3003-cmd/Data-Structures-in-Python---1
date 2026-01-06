81d775cd2a7083ad8e48d18b05eb0aa5c35a8989
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2026-01-06 18:49:16 +0700

    done

---
+++ demo.py
@@ -0,0 +1,51 @@
+fruits = ["Apple", "Kiwi", "Orange", "Banana", "Grape"]
+print(fruits[0])
+
+fruits.append("Melon")
+fruits.insert(2, "Lemon")
+print(fruits)
+
+fruits[2] = "Mango"
+fruits.remove("Banana")
+print(fruits)
+
+print(len(fruits))
+
+fruits.reverse()
+print(fruits)
+
+orange_fruits = fruits[2:4]
+print(orange_fruits)
+
+profile = {"name": "Enrico", "age": 14, "class": "Codingal"}
+print(profile)
+
+profile["name"] = "Bienve"
+print(profile)
+
+profile["country"] = "Indonesia"
+print(profile)
+
+del profile["country"]
+print(profile)
+print(profile.keys())
+print(profile.values())
+
+meeting = [
+    ["Ms", "Kshitun", "Teacher"],
+    [" ", "Enrico", "Student"]
+    [" ", "Bienve", "Student"]
+    [" ", "Saavi", "Student"]
+]
+
+meeting_dict = {}
+
+for participant in meeting:
+    title = participant[0]
+    name = participant[1]
+    role = participant[2]
+    meeting_dict = {"Name": title+name, "Role": role}
+
+print("Codingal Lesson: ")
+print(meeting_dict)
+    
