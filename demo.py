94adc70174d16554992710f537d6e1506b468d6e
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2026-01-09 18:52:58 +0700

    done 3

--- demo.py
+++ demo.py
@@ -48,4 +48,30 @@ for participant in meeting:
 
 print("Codingal Lesson: ")
 print(meeting_dict)
+
+import random
+
+num = random.randint(1, 100)
+guess = 0
+max_guess = 5
+attempts = 0
+
+print("The JUDGE has chosen.")
+print(f"Unfortunately, you only have {max_guess} attempts to guess the number.")
+
+while attempts < max_guess and guess != num:
+    guess = int(input("Proceed by providing your guess: "))
+    attempts += 1
     
+    if guess < num:
+        print("The number is too small!🤏")
+    elif guess > num:
+        print("The number is too large!👆")
+    else:
+        print("CONGRATULATIONS! YOU HAVE SUCCEEDED IN GUESSING THE NUMBER! 🤩💫")
+    
+    remaining = max_guess - attempts
+    print(f"Attempts remaining: {remaining}")
+
+if guess != num:
+    print(f"YOU HAVE FAILED. The correct number was {num}. 💀")
